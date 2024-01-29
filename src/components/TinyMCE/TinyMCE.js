b0d3f0e0df4e984134633f207920acb8c85ea234
Gintas <pikolas666@gmail.com>
2024-01-29 13:44:04 +0200

    removed tinymce plugins

--- src/components/TinyMCE/TinyMCE.js
+++ src/components/TinyMCE/TinyMCE.js
@@ -16,8 +16,7 @@ const TinyMCEModule = ({ initialValue, onEditorChange }) => {
 			init={{
 				// Your initialization options
 				selector: "#myTextarea",
-				plugins:
-					"ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss",
+				plugins: "",
 				toolbar:
 					"undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
 				tinycomments_mode: "embedded",
